d36375500cbc0386c34d88545ec8b924e73f024e
MasterDavodXLPRO <56481971+MasterDavodXLPRO@users.noreply.github.com>
2019-10-13 06:57:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const  client = new Discord.Client();
 const config = require("./config.json");
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
 
 client.on("ready", () => {
    console.log("Conectado!");
